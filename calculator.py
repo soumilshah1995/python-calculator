d39f20b163796e0fcca72cc23fea7d15522a57ac
soumil shah <soushah@my.bridgeport.edu>
2018-08-07 14:36:36 -0400

    FINAL GUI
    FINAL GUI

--- calculator.py
+++ calculator.py
@@ -9,10 +9,10 @@ Entry=tkinter.Entry(calculator,font=("Arial", 22), borderwidth= 1 ,)
 Entry.insert(0, "0")
 Entry.grid(row=0,column=0,columnspan=4)
 
+# ROW 1 --------------------------------------------------------------
 
-
-b_AC=tkinter.Button(calculator,text="AC",borderwidth=1,font=("Arial",20))
-b_AC.grid(row=1,column=0,sticky="NWNESWSE")
+b_AC=tkinter.Button(calculator,text="AC",borderwidth=1,font=("Arial",20),)
+b_AC.grid(row=1,column=0,sticky="NWNESWSE",)
 
 b_C=tkinter.Button(calculator,text="C",borderwidth=1,font=("Arial",20))
 b_C.grid(row=1,column=1,sticky="NWNESWSE")
@@ -24,7 +24,7 @@ b_divide=tkinter.Button(calculator,text="/",borderwidth=1,font=("Arial",20))
 b_divide.grid(row=1,column=3,sticky="NWNESWSE")
 
 
-
+# ROW 2 -------------------------------------------------------------------
 
 
 b_7=tkinter.Button(calculator,text="7",borderwidth=1,font=("Arial",20))
@@ -40,7 +40,41 @@ b_mul=tkinter.Button(calculator,text="*",borderwidth=1,font=("Arial",20))
 b_mul.grid(row=2,column=3,sticky="NWNESWSE")
 
 
+# ROW 3----------------------------------------------------------------------
 
 
+b_4=tkinter.Button(calculator,text="4",borderwidth=1,font=("Arial",20))
+b_4.grid(row=3,column=0,sticky="NWNESWSE")
+
+b_5=tkinter.Button(calculator,text="5",borderwidth=1,font=("Arial",20))
+b_5.grid(row=3,column=1,sticky="NWNESWSE")
+
+b_6=tkinter.Button(calculator,text="6",borderwidth=1,font=("Arial",20))
+b_6.grid(row=3,column=2,sticky="NWNESWSE")
+
+b_minus=tkinter.Button(calculator,text="-",borderwidth=1,font=("Arial",20))
+b_minus.grid(row=3,column=3,sticky="NWNESWSE")
+
+# ROW 4 ------------------------------------------------------------------------
+
+b_1=tkinter.Button(calculator,text="1",borderwidth=1,font=("Arial",20))
+b_1.grid(row=4,column=0,sticky="NWNESWSE")
+
+b_2=tkinter.Button(calculator,text="2",borderwidth=1,font=("Arial",20))
+b_2.grid(row=4,column=1,sticky="NWNESWSE")
+
+b_3=tkinter.Button(calculator,text="3",borderwidth=1,font=("Arial",20))
+b_3.grid(row=4,column=2,sticky="NWNESWSE")
+
+b_plus=tkinter.Button(calculator,text="+",borderwidth=1,font=("Arial",20))
+b_plus.grid(row=4,column=3,sticky="NWNESWSE")
+
+
+# last row
+b_0=tkinter.Button(calculator,text="0",font=("Arial",20),borderwidth= 1)
+b_0.grid(row=5,column=0,columnspan=3,sticky="NWNESWSE")
+
+b_equal=tkinter.Button(calculator,text="=",font=("Arial",20),borderwidth=1)
+b_equal.grid(row=5,column=3,sticky="NSNESWSE")
 
 calculator.mainloop()
