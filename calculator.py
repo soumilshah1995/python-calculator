53d3c3cce93deee0e1a83dbcb02c17b7b852786d
soumil shah <soushah@my.bridgeport.edu>
2018-08-07 13:16:06 -0400

    first change
    making of entry

--- calculator.py
+++ calculator.py
@@ -1,1 +1,12 @@
 import tkinter
+
+calculator= tkinter.Tk()
+calculator.title('Calculator')
+calculator.resizable(0, 0)
+
+
+Entry=tkinter.Entry(calculator,font=("Arial", 22), borderwidth= 1 ,)
+Entry.insert(0, "0")
+Entry.grid(row=0,column=0)
+
+calculator.mainloop()
